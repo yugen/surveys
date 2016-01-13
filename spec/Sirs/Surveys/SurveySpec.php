0ea410ad3e48442bab9b971519747d62fff0d6b4
TJ Ward <jward3@email.unc.edu>
2016-01-12 21:13:19 -0500

    Working on the parser

---
+++ spec/Sirs/Surveys/SurveySpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\Sirs\Surveys;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class SurveySpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Sirs\Surveys\Survey');
+    }
+}
--- src/Sirs/Surveys/SurveyParser.php
+++ src/Sirs/Surveys/SurveyParser.php
@@ -1,14 +1,18 @@
 <?php
 
-namespace Sirs\Survey;
+namespace Sirs\Surveys;
+
+use Sirs\Surveys\Survey;
 
 class SurveyParser
 {
-  protected $root;
+    protected $root;
+    protected $xml;
 
-    public function __construct($root)
+    public function __construct($root, $xml)
     {
       $this->root = $root ?: __DIR__.'../../';
+      $this->xml = new \SimpleXMLElement($xml);
     }
 
     public function fetchSurvey($file)
@@ -18,5 +22,28 @@ class SurveyParser
 
     protected function getSurveyPath($file){
         return $this->root.'/'.$file;
+    }
+
+    public function validate()
+    {
+        $doc = new \DOMDocument();
+        $doc->loadXML($this->xml->asXml());
+        return $doc->schemaValidate('schema/survey.xsd');
+    }
+
+    public function parse()
+    {
+        $survey = new Survey();
+        return $survey;
+        // TODO: write logic here
+    }
+
+    public function getPages()
+    {
+        $pages = [];
+        foreach($this->xml->page as $page){
+            $pages[] = new Page($page);
+        }
+        return $pages;
     }
 }
