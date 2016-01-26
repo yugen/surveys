04d82012b529011855e25636352b4077b59ed523
Alex Harding <alex.harding@unc.edu>
2016-01-26 16:58:54 -0500

    SM-33: Adding initial pass at router/controller

---
+++ src/Sirs/Surveys/SurveyController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Sirs-Survey\SurveyDocument;
+
+class SurveyController extends Controller
+{
+
+
+	/**
+	 * Checks if a Survey Response has already been created, and creates a new one if not. Retrieves appropriate page from Survey Document and renders it.
+	 *
+	 * @return view
+	 * @author SIRS
+	 **/
+    public function show( $respondentType, $respondentId, $surveyName, $pageName, $responseId = null){
+
+    	// get survey if response ID is populated
+    		if ( isset( $responseId ) {
+    			// get survey responses
+    			$response = "";
+    		}else{
+    			// check if response limit is met for this survey, if not then create new for given respondent
+    			$response = "";
+    		}
+
+    	// get survey object using surveyName and pageName
+    		$survey =  \SurveyDocument::initFromFile('directory/'.$surveyName.".xml");
+    		$page = $survey->getPageByName($pageName);
+    		return $page->render();
+
+    }
+
+	/**
+	 * saves input data from a survey page to SurveyResponse object, then hands off to route function
+	 *
+	 * @return redirect
+	 * @author SIRS
+	 **/
+    public function save( $respondentType, $respondentId, $surveyName, $pageName, $responseId, Request $request ){
+    	/** getting survey data back from the document **/
+    		$data = $request->all();
+
+    	/** TO DO: ADD VALIDATION AGAINST XML VALUES? **/
+
+    	/** instatiate rules doc **/
+    		$rulesdoc = $surveyName."Rules";
+    		$rules = new $rulesdoc;
+    		$method = $pageName."Save";
+
+    	/** instatiating response **/
+    		$response = SurveyResponse::firstOrFail($responseId);
+
+    		if ( method_exists( $rules, $method ) ) {
+    			// there is a custom rule for save here
+    			call_user_func($rules, $method);
+    		}else{
+    			// there is not a custom rule for save here
+    			foreach ($data as $key => $value) {
+    				$response->$key = $value;
+	    		}
+	    		$response->save();
+    		}	
+
+    	/** getting navigation direction and passing all data on to the router, so that we don't have to re-do queries **/
+    		$nav = $data['nav'];
+    		$this->route($nav, $response, $rules, $respondentType, $respondentId, $surveyName, $pageName, $responseId);
+    }
+
+
+    /**
+	 * checks if any custom page logic has been created, moves to next page in direction if not
+	 *
+	 * @return redirect
+	 * @author SIRS
+	 **/
+    public function route($nav, $response, $rules, $respondentType, $respondentId, $surveyName, $pageName, $responseId){
+
+    	/**instatiating survey document and getting pages **/
+	    	$survey =  \SurveyDocument::initFromFile('directory/'.$surveyName.".xml");
+	    	$pageIndex = $survey->getPageIndexByName($pageName);
+	    	// get page name and find next page using the nav
+	    	if ($nav = 'next') {
+	    		$nav = 1;
+	    	} elseif ($nav = 'prev') {
+	    		$nav = -1;
+	    	}
+	    	$pageIndex += $nav;
+	    	$target = $survey->pages[$pageIndex]->name;
+	    	$method = $target."Skip";
+	    /** looking for custom skip method, deciding what to do with response **/
+	    	if ( method_exists( $rules, $method ) ) {
+	    		$skip = call_user_func($rules, $method);
+	    		switch ($skip) {
+	    			case 0:
+	    				// we are not skipping
+	    				return redirect( action( 'SurveyController@show',[ $respondentType, $respondentId, $surveyName, $pageName, $responseId ] ) );
+	    				break;
+	    			case 1:
+	    				// we are skipping this page
+	    				$pageIndex = $survey->getPageIndexByName($target);
+	    				$pageIndex += $nav;
+	    				$pageName = $survey->pages[$pageIndex]->name;
+	    				$this->route($nav, $response, $rules, $respondentType, $respondentId, $surveyName, $pageName, $responseId);
+	    				break;
+	    			case 2:
+	    				// we are finalizing
+	    				$response->finalize();
+	    				break;
+	    			default:
+	    				Throw new InvalidInputException("Invalid value returned in ".$target);
+	    				break;
+	    		}
+
+	    	} else{
+	    		// no custom method
+	    		$pageName = $target;
+	    		return redirect( action( 'SurveyController@show',[ $respondentType, $respondentId, $surveyName, $pageName, $responseId ] ) );
+	    	}
+    }
+
+}
