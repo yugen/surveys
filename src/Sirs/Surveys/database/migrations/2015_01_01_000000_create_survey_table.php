2468621ed4a5085f0a8e0be9b8a95184b6daea75
TJ Ward <jward3@email.unc.edu>
2016-01-28 13:42:11 -0500

    adding survey table migration and .hgignore

---
+++ src/Sirs/Surveys/database/migrations/2015_01_01_000000_create_survey_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveysTable extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('surveys', function(Blueprint $table)
+    {
+      $table->increments('id')->unsigned();
+      $table->string('name');
+      $table->string('version');
+      $table->string('slug')->unique();
+      $table->text('file_path')->unique();
+      $table->timestamps();
+
+      $table->unique(['name', 'version']);
+      $table->index('slug');
+    });
+
+
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('surveys');
+  }
+
+}
