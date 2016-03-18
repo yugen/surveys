37ca22fa4963b070ad8166e0de2c5eed97266b6c
Alex Harding <alex.harding@unc.edu>
2016-03-18 10:30:08 -0400

    Re-ordering routes

--- src/Sirs/Surveys/routes.php
+++ src/Sirs/Surveys/routes.php
@@ -2,7 +2,7 @@
 
 $routeGroup = (config('surveys.routeGroup')) ? config('surveys.routeGroup') : []; 
 Route::group($routeGroup, function(){
-  Route::get('{respondentId}/{respondentType}/survey/{surveySlug}/{responseId?}', [
+  Route::get('{respondentType}/{respondentId}/survey/{surveySlug}/{responseId?}', [
     'uses' => 'Sirs\Surveys\Controllers\SurveyController@show',
     'as' => 'survey_get'
   ]);
