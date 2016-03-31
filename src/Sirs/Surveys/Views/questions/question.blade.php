55b377912950d256ef7157dcbf83b3de21e8ef08
TJ Ward <jward3@email.unc.edu>
2016-03-31 12:57:58 -0400

    added question.blade.php for markup reuse

---
+++ src/Sirs/Surveys/Views/questions/question.blade.php
@@ -0,0 +1,18 @@
+<div 
+  class="
+    form-group question-block
+    {{($renderable->class) ? ' '.$renderable->class : ''}} 
+    @if(isset($context['errors']) && $context['errors']->has($renderable->name)) has-errors @endif
+  "
+  id="{{$renderable->id or ''}}"
+>
+  @if($renderable->questionText)
+    <div class="question-text">{{$renderable->questionText}}</div>
+  @endif
+  <div class="row">
+    <div class="question-answers col-sm-9">
+      @yield('answers')
+    </div>
+    <div class="col-sm-3">@include('error')</div>
+  </div>
+</div>  
