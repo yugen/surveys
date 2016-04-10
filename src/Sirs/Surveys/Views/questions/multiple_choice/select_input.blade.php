ee48b737ee003d7742342f74104193bb02b39c1f
TJ Ward <jward3@email.unc.edu>
2016-04-10 13:04:57 -0400

    added select option with empty value to select_input.blade.php

--- src/Sirs/Surveys/Views/questions/multiple_choice/select_input.blade.php
+++ src/Sirs/Surveys/Views/questions/multiple_choice/select_input.blade.php
@@ -6,23 +6,24 @@
   "
   {{($question->required) ? ' required' : ''}}
 >
-      @foreach($question->options as $option)
-        <option 
-          id="{{$question->name}}_{{$option->value}}" 
-          value="{{ $option->value }}"
-          
-          autocomplete="off"
-          @if($context['response']->{$question->name} == $option->value)
-            selected="selected"
-          @endif
-          @if($option->show)
-            data-skipTarget="{{$option->show}}"
-          @endif
-          @if($option->hide)
-            data-hide="{{$option->hide}}"
-          @endif
-         />
-         {{$option->label}}
-       </option>
-      @endforeach
+    <option value="">Select...</option>
+    @foreach($question->options as $option)
+      <option 
+        id="{{$question->name}}_{{$option->value}}" 
+        value="{{ $option->value }}"
+        
+        autocomplete="off"
+        @if($context['response']->{$question->name} == $option->value)
+          selected="selected"
+        @endif
+        @if($option->show)
+          data-skipTarget="{{$option->show}}"
+        @endif
+        @if($option->hide)
+          data-hide="{{$option->hide}}"
+        @endif
+       />
+       {{$option->label}}
+     </option>
+    @endforeach
 </select>
