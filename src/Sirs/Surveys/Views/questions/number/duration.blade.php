63f092a56fe4052416f862b66ebba4e66eb3e251
TJ Ward <jward3@email.unc.edu>
2016-04-14 13:51:40 -0400

    made duration a text input

--- src/Sirs/Surveys/Views/questions/number/duration.blade.php
+++ src/Sirs/Surveys/Views/questions/number/duration.blade.php
@@ -1,7 +1,7 @@
 @extends('questions.question')
 @section('answers')
   <div class="input-group">
-    @include('questions.input', ['question'=>$renderable, 'context'=>$context, 'type'=>'number'])
+    @include('questions.input', ['question'=>$renderable, 'context'=>$context, 'type'=>'text'])
     <span class="input-group-addon">{{$renderable->unit}}s</span>
   </div>
  @endsection
