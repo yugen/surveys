21cd319faa8fdae4f41356c49571632f3c8115dc
TJ Ward <jward3@email.unc.edu>
2016-04-19 23:12:40 -0400

    fixed duration_hourmin styles.

--- src/Sirs/Surveys/Views/questions/number/duration_hourmin.blade.php
+++ src/Sirs/Surveys/Views/questions/number/duration_hourmin.blade.php
@@ -1,21 +1,20 @@
 @extends('questions.question')
 @section('answers')
   <div class="input-group">
-      
     <input type="text" id="{{$renderable->name}}_hours" 
       class="form-control"
       @if(method_exists($renderable, 'getMin') && $renderable->min)
-      min="{{$renderable->min / 60}}"
-    @endif
-    @if(method_exists($renderable, 'getMax') && $renderable->max)
-      max="{{floor($renderable->max / 60)}}"
-    @endif
-    {{($renderable->required) ? ' required' : ''}}
-     @if($context['response']->{$renderable->name} != -77 && !empty($context['response']->{$renderable->name}) )
+        min="{{$renderable->min / 60}}"
+      @endif
+      @if(method_exists($renderable, 'getMax') && $renderable->max)
+        max="{{floor($renderable->max / 60)}}"
+      @endif
+      {{($renderable->required) ? ' required' : ''}}
+      @if($context['response']->{$renderable->name} != -77 && !empty($context['response']->{$renderable->name}))
         value="{{  isset($context['response']->{$renderable->name}) ? (floor($context['response']->{$renderable->name}/60)) : '' }}"
       @endif
     />
-    <span class="input-group-addon" >hours</span>
+    <span class="input-group-addon">hours</span>
     <input type="text"  id="{{$renderable->name}}_minutes" 
       class="form-control"
       @if(method_exists($renderable, 'getMin') && $renderable->min)
@@ -29,26 +28,18 @@
           value="{{ isset($context['response']->{$renderable->name}) ? ($context['response']->{$renderable->name} % 60) : '' }}"
         @endif
     />
-    <span class="input-group-addon" >minutes</span>
-    @include('questions.input', ['question'=>$renderable, 'context'=>$context, 'type'=>'hidden'])
+    <span class="input-group-addon">minutes</span>
   </div>
+  @include('questions.input', ['question'=>$renderable, 'context'=>$context, 'type'=>'hidden'])
   <script>
     document.getElementById('{{$renderable->name}}_hours').addEventListener('change', function(evt){
       elem = document.getElementsByName('{{$renderable->name}}')[0];
-
       elem.value = ((parseInt(document.getElementById('{{$renderable->name}}_hours').value) * 60) || 0) + (parseInt(document.getElementById('{{$renderable->name}}_minutes').value) ||0);
-      //elem.fireEvent("onchange");
-
-        
     });
 
-  document.getElementById('{{$renderable->name}}_minutes').addEventListener('change', function(evt){
-      elem = document.getElementsByName('{{$renderable->name}}')[0];
-      elem.value = ((parseInt(document.getElementById('{{$renderable->name}}_hours').value) * 60) || 0) + (parseInt(document.getElementById('{{$renderable->name}}_minutes').value) ||0);
-      //elem.fireEvent("onchange");
-      
-
-        
+    document.getElementById('{{$renderable->name}}_minutes').addEventListener('change', function(evt){
+        elem = document.getElementsByName('{{$renderable->name}}')[0];
+        elem.value = ((parseInt(document.getElementById('{{$renderable->name}}_hours').value) * 60) || 0) + (parseInt(document.getElementById('{{$renderable->name}}_minutes').value) ||0);
     });
   </script>
  @endsection
