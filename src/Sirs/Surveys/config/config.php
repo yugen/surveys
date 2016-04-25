b48460c9141b194771de79cd34f8503a74e313e0
TJ Ward <jward3@email.unc.edu>
2016-04-25 14:52:02 -0400

    updated survey config to remove defaultTemplatePath

--- src/Sirs/Surveys/config/config.php
+++ src/Sirs/Surveys/config/config.php
@@ -3,7 +3,6 @@
 return [
   'surveysPath' => base_path('resources/surveys'),
   'rulesPath' => app_path('Surveys'),
-  'defaultTemplatePath' => __DIR__.'/../Views/',
   'customTemplatePath' => base_path('resources/views/surveys'),
   'rendererConfig' => [
     'cache_path' => storage_path('storage/framework/views'),
