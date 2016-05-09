a8c5fc2cec59b7ccae7ede4b141ea223e7322e6b
Josh Knop <jknop@unc.edu>
2016-05-09 12:42:37 -0400

    duration is now only saved if both hours and minutes are entered

--- src/Sirs/Surveys/Views/questions/number/duration_hourmin.blade.php
+++ src/Sirs/Surveys/Views/questions/number/duration_hourmin.blade.php
@@ -29,20 +29,24 @@
        @if($context['response']->{$renderable->name} != -77 && 
           $context['response']->{$renderable->name} !== null &&
           $context['response']->{$renderable->name} !== '' )
-          value="{{ ($context['response']->{$renderable->name} !== null) ? ($context['response']->{$renderable->name} % 60) : '' }}"
+          value="{{ ($context['response']->{$renderable->name} !== null) ? ($context['response']->{$renderable->name} % 60) : ''  }}"
         @endif
     />
+
     <span class="input-group-addon">minutes</span>
   </div>
   @include('questions.input', ['question'=>$renderable, 'context'=>$context, 'type'=>'hidden'])
+
+
   <script>
     // in a self-invoking function to constrain scope
     (function(){
       var hiddenInput = document.getElementsByName('{{$renderable->name}}')[0],
           hoursInput = document.getElementById('{{$renderable->name}}_hours'),
           minInput = document.getElementById('{{$renderable->name}}_minutes');
+      hiddenInput.id = hiddenInput.name;
       var setInput = function(){
-        if (hoursInput.value != '' || minInput.value != '') {
+        if (hoursInput.value != '' && minInput.value != '') {
           hiddenInput.value = ((parseInt(hoursInput.value) * 60) || 0) + (parseInt(minInput.value) || 0);
         }else{
           hiddenInput.value = null;
