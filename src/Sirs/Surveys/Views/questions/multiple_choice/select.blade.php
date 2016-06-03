53f34e11f44cc62aacf815ccea372e13ba206738
TJ Ward <jward3@email.unc.edu>
2016-06-03 13:03:11 -0400

    updated select.blade.php to extend question.

--- src/Sirs/Surveys/Views/questions/multiple_choice/select.blade.php
+++ src/Sirs/Surveys/Views/questions/multiple_choice/select.blade.php
@@ -1,18 +1,10 @@
-<div 
-  class="
-    form-group question-block
-    {{($renderable->class) ? ' '.$renderable->class : ''}} 
-    @if(isset($context['errors']) && $context['errors']->has($renderable->name)) has-errors @endif
-  "
-  id="{{$renderable->id or ''}}"
->
-  @if($renderable->questionText)
-    <div class="question-text">{{$renderable->questionText}}</div>
-  @endif
+@extends('questions.question')
+
+@section('answers')
   <div class="row">
     <div class="question-answers col-sm-9">
       @include('questions.multiple_choice.select_input', ['question'=>$renderable, 'context'=>$context])
     </div>
     <div class="col-sm-3">@include('error', ['question'=>$renderable])</div>
   </div>
-</div>  
+@endsection
