db81a8fbfaef94e9ea0d53eb419c2bf6eb98b6b6
TJ Ward <jward3@email.unc.edu>
2016-11-16 10:43:00 -0500

    extracted var list from response data view for re-use

--- src/Sirs/Surveys/Views/responses/detail.blade.php
+++ src/Sirs/Surveys/Views/responses/detail.blade.php
@@ -74,62 +74,6 @@
 
 <hr>
 <div style="height: 500px; overflow-y: scroll;">
-@foreach($response->survey->surveyDocument->pages as $page)
-    <div class="page-container" id="{{snake_case($page->name)}}">
-        <h3>{{$page->title}}</h3>
-        @if(count($page->getQuestions()) > 0)
-        <table class="table table-striped table-bordered">
-            <tr>
-                <th style="width: 20%">Variable Name</th>
-                <th style="width: 50%">Question Text</th>
-                <th style="width: 20%">Value</th>
-                <th style="width: 10%">Raw Value</th>
-            </tr>
-            @foreach($page->getQuestions() as $question)
-            <tr>
-                <td>{{$question->name}}</td>
-                <td>{!! $question->questionText !!}</td>
-                <td>
-                    @if($question->hasOptions())
-                        <ul class="list-unstyled">
-                        @if($question->numSelectable > 1)
-                            @foreach($question->getVariables() as $var)
-                                @if($response->{$var->name} == 1)
-                                    @foreach($question->options as $option)
-                                        {{-- option: {{$option->name}}<br /> --}}
-                                        @if($option->name == $var->name)
-                                            <li>{{ $option->label }}</li>
-                                        @endif
-                                    @endforeach
-                                @endif
-                            @endforeach
-                        @else
-                            @foreach($question->getOptionsForResponseValue($response->{$question->name}) as $option)
-                                <li>{{$option->label}}</li>
-                            @endforeach
-                        @endif
-                        </ul>
-                    @else
-                        @if($response->{$question->name})
-                            {{($response->{$question->name} == -77) ? config('surveys.refuseLabel', 'Refused') : $response->{$question->name} }}
-                        @endif 
-                    @endif
-                </td>
-                <td>
-                    @if(!$question->hasOptions())
-                        {{$response->{$question->name} or 'null'}}
-                    @elseif( $question->hasOptions())
-                        {{$response->{$question->name} or 'null'}}
-                    @else
-                        --
-                    @endif
-                </td>
-            </tr>
-            @endforeach
-        </table>     
-        @else
-        <div class="well">This page does not have any questions.</div>
-        @endif
-    @endforeach
+    @include('surveys::responses.var_list', ['response'=>$response])
 </div>
 @endsection
---
+++ src/Sirs/Surveys/Views/responses/var_list.blade.php
@@ -0,0 +1,62 @@
+@foreach($response->survey->surveyDocument->pages as $page)
+<div class="page-container" id="{{snake_case($page->name)}}">
+    <h3>
+        {{$page->title}}
+        @if(count($page->getQuestions()) > 0)
+            <a href="{{route('survey_get', [get_class($response->respondent), $response->respondent->id, $response->survey->slug, $response->id])}}?page={{$page->name}}" class="btn btn-xs btn-default pull-right">Edit Page</a>
+        @endif
+    </h3>
+    @if(count($page->getQuestions()) > 0)
+    <table class="table table-striped table-bordered">
+        <tr>
+            <th style="width: 20%">Variable Name</th>
+            <th style="width: 50%">Question Text</th>
+            <th style="width: 20%">Value</th>
+            <th style="width: 10%">Raw Value</th>
+        </tr>
+        @foreach($page->getQuestions() as $question)
+        <tr>
+            <td>{{$question->name}}</td>
+            <td>{!! $question->questionText !!}</td>
+            <td>
+                @if($question->hasOptions())
+                    <ul class="list-unstyled">
+                    @if($question->numSelectable > 1)
+                        @foreach($question->getVariables() as $var)
+                            @if($response->{$var->name} == 1)
+                                @foreach($question->options as $option)
+                                    {{-- option: {{$option->name}}<br /> --}}
+                                    @if($option->name == $var->name)
+                                        <li>{{ $option->label }}</li>
+                                    @endif
+                                @endforeach
+                            @endif
+                        @endforeach
+                    @else
+                        @foreach($question->getOptionsForResponseValue($response->{$question->name}) as $option)
+                            <li>{{$option->label}}</li>
+                        @endforeach
+                    @endif
+                    </ul>
+                @else
+                    @if($response->{$question->name})
+                        {{($response->{$question->name} == -77) ? config('surveys.refuseLabel', 'Refused') : $response->{$question->name} }}
+                    @endif 
+                @endif
+            </td>
+            <td>
+                @if(!$question->hasOptions())
+                    {{$response->{$question->name} or 'null'}}
+                @elseif( $question->hasOptions())
+                    {{$response->{$question->name} or 'null'}}
+                @else
+                    --
+                @endif
+            </td>
+        </tr>
+        @endforeach
+    </table>     
+    @else
+        <div class="well">This page does not have any questions.</div>
+    @endif
+@endforeach
