a2e59d892655188fc300ba285e2f9e08cd37f306
TJ Ward <jward3@email.unc.edu>
2017-02-17 14:34:27 -0500

    Add note to page when response finalized.

--- src/Sirs/Surveys/Views/containers/page/page.blade.php
+++ src/Sirs/Surveys/Views/containers/page/page.blade.php
@@ -1,11 +1,19 @@
 @extends($chromeTemplate)
 
 @section('content')
+
 <form class="sirs-survey" method="POST" name="{{$context['survey']['name']}}-{{$renderable->name}}" novalidate>
   <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
   <div class="panel panel-default">
     <div class="panel-heading">
       <div class="pull-right" style="margin-top: 4px;">
+        
+        @if($context['response']->finalized_at)
+          <small class="finalized-response-warning">
+            <strong>This response was finalized on {{$context['response']->finalized_at->format('m/d/Y')}}</strong>
+          </small>
+        @endif
+
         <a href="{{route('surveys.{surveySlug}.responses.show', [$context['survey']['object']->slug, $context['response']->id])}}" class="btn btn-sm btn-default">View Data</a>
       </div>
       <h4>
@@ -15,7 +23,6 @@
         - 
         {{ $context['survey']['title'] or ucwords($context['survey']['name'])}}        
         - {{$renderable->title}}
-        <small class="pull-right">v.{{$context['survey']['version']}}</small>
       </h4>
     </div>
     <div class="panel-body">
@@ -51,6 +58,13 @@
   </div>
 </form>
 <div class="alert alert-info notification" id="flast-notification">Auto-saved at <span id="notification-time"></span>.</div>
+<div class="text-muted">
+  <small>
+    {{ $context['survey']['title'] or ucwords($context['survey']['name'])}}        
+    - {{$renderable->title}}
+    version {{$context['survey']['version']}}
+  </small>
+</div>
 @endsection
 
 @push('styles')
