85cc2c74a53e7abbabfb663f87ac8487e4b46152
jward3 <devnull@localhost>
2017-06-21 15:52:07 -0400

    add workflow strategies for surveys

---
+++ src/Sirs/Surveys/Handlers/RunWorkflow.php
@@ -0,0 +1,40 @@
+<?php 
+namespace Sirs\Surveys\Handlers;
+use Bus;
+use Illuminate\Contracts\Queue\ShouldBeQueued;
+use Illuminate\Queue\InteractsWithQueue;
+use Sirs\Surveys\Events\SurveyResponseEvent;
+
+/**
+ * Event Listener that calls appropriate workflow strategy if it exists
+ */
+class RunWorkflow {
+
+  /**
+   * Create the event handler.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    //
+  }
+
+  /**
+   * Handle the event.
+   *
+   * @param  Sirs\Surveys\Events\AppointmentEvent  $event
+   * @return void
+   */
+  public function handle(SurveyResponseEvent $event)
+  {
+    $response = $event->surveyResponse;
+
+    $workflowClassName = 'App\\Surveys\\'.ucfirst(camel_case($response->survey->slug).'WorkflowStrategy');
+    if (class_exists($workflowClassName)) {
+      $workflow = new $workflowClassName($response, $event);
+      $workflow->run();
+    }
+  }
+
+}
--- src/Sirs/Surveys/SurveysServiceProvider.php
+++ src/Sirs/Surveys/SurveysServiceProvider.php
@@ -9,6 +9,7 @@ use Sirs\Surveys\Console\CreateSurveyMigrationsFromDocument;
 use Sirs\Surveys\Console\CreateSurveyRules;
 use Sirs\Surveys\Console\CreateSurveyRulesFromDocument;
 use Sirs\Surveys\Console\CreateSurveyXml;
+use Sirs\Surveys\Console\CreateWorkflowStrategy;
 use Sirs\Surveys\Console\NewSurveyFromDocument;
 use Sirs\Surveys\Console\ValidateSurveyDefinition;
 use Sirs\Surveys\Models\Response;
@@ -26,46 +27,13 @@ class SurveysServiceProvider extends ServiceProvider {
 
     $this->publishes([ __DIR__.'/config/config.php' => config_path('surveys.php') ], 'config');
     $this->publishes([ __DIR__.'/database/migrations/' => database_path('/migrations') ], 'migrations');
-    $this->publishes([ __DIR__.'/database/migrations/' => database_path('/migrations') ], 'migrations');
+    // $this->publishes([ __DIR__.'/database/migrations/' => database_path('/migrations') ], 'migrations');
     $this->publishes([ __DIR__.'/../../../assets/sass/'=> base_path('/resources/assets/sass')], 'sass');
     $this->publishes([ __DIR__.'/../../../assets/js/'=> public_path('/js')], 'js');
     $this->publishes([ __DIR__.'/Policies/'=> app_path('Policies')], 'policies');
 
-
-    // custom validators for numbers
-    Validator::replacer('intMin', function($message, $attribute, $rule, $parameters) {
-      if($message == 'validation.int_min'){
-        $message = 'Must be at least :min';
-      }
-      return str_replace(':min', $parameters[0], 'Must be greater than or equal to :min');
-    });
-    Validator::extend('intMin', function($attribute, $value, $parameters, $validator) {
-      return ((int)$value >= (int)$parameters[0]);
-    });
-    Validator::replacer('intMin', function($message, $attribute, $rule, $parameters) {
-      if($message == 'validation.int_min'){
-        $message = 'Must be at least :min';
-      }
-      return str_replace(':min', $parameters[0], 'Must be greater than or equal to :min');
-    });
-    Validator::extend('refusableIntMin', function($attribute, $value, $parameters, $validator) {
-      return ((int)$value >= (int)$parameters[0] || (int)$value == -77);
-    });
-    Validator::replacer('refusableIntMin', function($message, $attribute, $rule, $parameters) {
-      if($message == 'validation.refusable_int_min'){
-        $message = 'Must be greater than or equal to :min or -77 (refused)';
-      }
-      return str_replace(':min', $parameters[0], 'Must be greater than or equal to :min or -77 (refused)');
-    });
-    Validator::extend('intMax', function($attribute, $value, $parameters, $validator) {
-      return ((int)$value <= (int)$parameters[0]);
-    });
-    Validator::replacer('intMax', function($message, $attribute, $rule, $parameters) {
-      if($message == 'validation.int_max'){
-        $message = 'Must be at less than or equal to :max';
-      }
-      return str_replace(':max', $parameters[0], $message);
-    });
+    $this->addValidators();
+    $this->addWorkflowListeners();
 
     // register observers:
     Response::observe(new SurveyResponseObserver);    
@@ -81,35 +49,7 @@ class SurveysServiceProvider extends ServiceProvider {
    */
   public function register()
   {
-    $this->app->singleton('command.survey.migration', function($app) {
-      return new CreateSurveyMigrationsFromDocument();
-    });
-    $this->commands('command.survey.migration');
-
-    $this->app->singleton('command.survey.rules', function($app) {
-      return new CreateSurveyRulesFromDocument();
-    });
-    $this->commands('command.survey.rules');
-
-    $this->app->singleton('command.survey.validate', function($app) {
-      return new ValidateSurveyDefinition();
-    });
-    $this->commands('command.survey.validate');
-
-    $this->app->singleton('command.survey.new', function($app) {
-      return new NewSurveyFromDocument();
-    });
-    $this->commands('command.survey.new');
-
-    $this->app->singleton('command.make.survey-rules', function($app) {
-      return new CreateSurveyRules();
-    });
-    $this->commands('command.make.survey-rules');
-
-
-    $this->commands([
-        CreateSurveyXml::class,
-    ]);
+    $this->registerCommands();
   }
 
   /**
@@ -125,5 +65,86 @@ class SurveysServiceProvider extends ServiceProvider {
         ];
     }
 
+    protected function addValidators()
+    {
+      // custom validators for numbers
+      Validator::replacer('intMin', function($message, $attribute, $rule, $parameters) {
+        if($message == 'validation.int_min'){
+          $message = 'Must be at least :min';
+        }
+        return str_replace(':min', $parameters[0], 'Must be greater than or equal to :min');
+      });
+      Validator::extend('intMin', function($attribute, $value, $parameters, $validator) {
+        return ((int)$value >= (int)$parameters[0]);
+      });
+      Validator::replacer('intMin', function($message, $attribute, $rule, $parameters) {
+        if($message == 'validation.int_min'){
+          $message = 'Must be at least :min';
+        }
+        return str_replace(':min', $parameters[0], 'Must be greater than or equal to :min');
+      });
+      Validator::extend('refusableIntMin', function($attribute, $value, $parameters, $validator) {
+        return ((int)$value >= (int)$parameters[0] || (int)$value == -77);
+      });
+      Validator::replacer('refusableIntMin', function($message, $attribute, $rule, $parameters) {
+        if($message == 'validation.refusable_int_min'){
+          $message = 'Must be greater than or equal to :min or -77 (refused)';
+        }
+        return str_replace(':min', $parameters[0], 'Must be greater than or equal to :min or -77 (refused)');
+      });
+      Validator::extend('intMax', function($attribute, $value, $parameters, $validator) {
+        return ((int)$value <= (int)$parameters[0]);
+      });
+      Validator::replacer('intMax', function($message, $attribute, $rule, $parameters) {
+        if($message == 'validation.int_max'){
+          $message = 'Must be at less than or equal to :max';
+        }
+        return str_replace(':max', $parameters[0], $message);
+      });
+    }
+
+    protected function registerCommands()
+    {
+      $this->app->singleton('command.survey.migration', function($app) {
+        return new CreateSurveyMigrationsFromDocument();
+      });
+      $this->commands('command.survey.migration');
+
+      $this->app->singleton('command.survey.rules', function($app) {
+        return new CreateSurveyRulesFromDocument();
+      });
+      $this->commands('command.survey.rules');
+
+      $this->app->singleton('command.survey.validate', function($app) {
+        return new ValidateSurveyDefinition();
+      });
+      $this->commands('command.survey.validate');
+
+      $this->app->singleton('command.survey.new', function($app) {
+        return new NewSurveyFromDocument();
+      });
+      $this->commands('command.survey.new');
+
+      $this->app->singleton('command.make.survey-rules', function($app) {
+        return new CreateSurveyRules();
+      });
+      $this->commands('command.make.survey-rules');
+
+
+      $this->commands([
+          CreateSurveyXml::class,
+          CreateWorkflowStrategy::class
+      ]);
+    }
+
+    protected function addWorkflowListeners()
+    {
+      \Event::listen('Sirs\Surveys\Events\SurveyResponseFinalized', 'Sirs\Surveys\Handlers\RunWorkflow');
+      \Event::listen('Sirs\Surveys\Events\SurveyResponseReopened', 'Sirs\Surveys\Handlers\RunWorkflow');
+      \Event::listen('Sirs\Surveys\Events\SurveyResponseSaved', 'Sirs\Surveys\Handlers\RunWorkflow');
+      \Event::listen('Sirs\Surveys\Events\SurveyResponseStarted', 'Sirs\Surveys\Handlers\RunWorkflow');
+
+
+    }
 
 }
