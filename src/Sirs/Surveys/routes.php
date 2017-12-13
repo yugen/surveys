c063017e5f71f03b376008629fd9a179241595b9
jward3 <devnull@localhost>
2017-12-13 18:18:31 -0500

    Add prefix to survey routes

--- src/Sirs/Surveys/routes.php
+++ src/Sirs/Surveys/routes.php
@@ -1,57 +1,59 @@
 <?php
 
-$routeGroup = (config('surveys.routeGroup')) ? config('surveys.routeGroup') : []; 
+$routeGroup = (config('surveys.routeGroup')) ? config('surveys.routeGroup') : [];
 
-Route::group($routeGroup, function(){
-  Route::get('
-    {respondentType}/{respondentId}/survey/{surveySlug}/{responseId?}', 
+$routeGroup['prefix'] = 'surveys';
+$routeGroup['as'] = 'surveys.';
+
+Route::group($routeGroup, function () {
+    Route::get(
+      '
+    {respondentType}/{respondentId}/survey/{surveySlug}/{responseId?}',
     [
       'uses' => 'Sirs\Surveys\Controllers\SurveyController@show',
       'as' => 'survey_get'
     ]
   );
   
-  Route::put(
-    'api/{respondentType}/{respondentId}/survey/{surveySlug}/{responseId?}', 
+    Route::put(
+    'api/{respondentType}/{respondentId}/survey/{surveySlug}/{responseId?}',
     [
       'uses' => 'Sirs\Surveys\Controllers\SurveyController@autosave',
       'as'=>'surveys.autosave'
     ]
   );
 
-  Route::post(
-    '{respondentType}/{respondentId}/survey/{surveySlug}/{responseId?}', 
+    Route::post(
+    '{respondentType}/{respondentId}/survey/{surveySlug}/{responseId?}',
     [
       'uses' => 'Sirs\Surveys\Controllers\SurveyController@store',
       'as' => 'survey_post'
     ]
   );
   
-  Route::resource(
-    'surveys/{surveySlug}/responses', 
+    Route::resource(
+    'surveys/{surveySlug}/responses',
     'Sirs\Surveys\Controllers\ResponseController'
   );
 
-  Route::get(
-    'surveys/reports', 
+    Route::get(
+    'surveys/reports',
     'Sirs\Surveys\Controllers\ReportController@index'
   );
   
-  Route::any(
-    'surveys/{surveySlug}/report/detail/{pageName?}/{variableName?}', 
+    Route::any(
+    'surveys/{surveySlug}/report/detail/{pageName?}/{variableName?}',
     'Sirs\Surveys\Controllers\ReportController@detail'
   );
   
-  Route::any(
-    'surveys/{surveySlug}/report', 
+    Route::any(
+    'surveys/{surveySlug}/report',
     'Sirs\Surveys\Controllers\ReportController@overview'
   );
   
-  Route::resource(
-    'surveys/data-dictionary', 
-    'Sirs\Surveys\Controllers\DictionaryController', 
+    Route::resource(
+    'surveys/data-dictionary',
+    'Sirs\Surveys\Controllers\DictionaryController',
     ['only'=>['index','show']]
   );
 });
-
-?>
