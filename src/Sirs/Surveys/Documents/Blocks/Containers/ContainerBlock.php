e43b9813416b70ab12737f1e63b2cb63407fb633
jward3 <jward3@email.unc.edu>
2018-01-10 10:24:57 -0500

    Add name to any Container's child blocks that don't have one for indexing

--- src/Sirs/Surveys/Documents/Blocks/Containers/ContainerBlock.php
+++ src/Sirs/Surveys/Documents/Blocks/Containers/ContainerBlock.php
@@ -9,112 +9,123 @@ use Sirs\Surveys\HasQuestionsTrait;
 
 class ContainerBlock extends RenderableBlock implements ContainerInterface
 {
-  use HasQuestionsTrait;
+    use HasQuestionsTrait;
 
-  protected $name;
-  protected $contents;
-  /**
-   * construct a new container
-   *
-   * @return void
-   * @param string $name name
-   * @param array $contents
-   * @param string $class class name used in rendering
-   * @param string $id id used in rendering
-   * @param string $template template path
-   **/
-  function __construct($xml = null){
-    $this->contents = [];
-    parent::__construct($xml);
-  }
-
-  function parse(\SimpleXMLElement $simpleXmlElement)
-  {
-    $this->setName($this->getAttribute($simpleXmlElement, 'name'));
-    parent::parse($simpleXmlElement);
-    // foreach children do the right thing
-    $this->setContents($this->parseContents($simpleXmlElement));
-    return $this;
-  }
-
-  function parseContents(\SimpleXMLElement $simpleXmlElement){
-    $blockFactory = new BlockFactory();
-    $children = [];
-    foreach($simpleXmlElement->children() as $child){
-      if ( in_array( $child->getName(), $blockFactory->getWhitelist() ) ) {
-        $childClass = $blockFactory->getBlockClass($child);
-        $childBlock = $childClass::createWithParameters($child, $this->getParameters());
-        $children[$childBlock->name] = $childBlock;
-      }
+    protected $name;
+    protected $contents;
+    /**
+     * construct a new container
+     *
+     * @return void
+     * @param string $name name
+     * @param array $contents
+     * @param string $class class name used in rendering
+     * @param string $id id used in rendering
+     * @param string $template template path
+     **/
+    public function __construct($xml = null)
+    {
+        $this->contents = [];
+        parent::__construct($xml);
     }
-    return $children;
-  }
 
-  /**
-   * sets the name if any
-   *
-   * @return Sirs\Survey\Container $this
-   **/
-  function setName($name){
-    $this->name = $name;
-    return $this;
-  }
-
-  /**
-   * gets name
-   *
-   * @return string
-   **/
-  function getName(){
-    return $this->name;
-  }
-
-  /**
-   * Sets the contents of the container
-   *
-   * @param array $contents - array of contents
-   * @return Sirs\Survey\Container $this
-   **/
-  public function setContents(array $contents){
-    $this->contents = $contents;
-    return $this;
-  }
-
-  /**
-   * Gets the contents of this container
-   *
-   * @return array - Array of contents
-   **/
-  public function getContents(){
-    return $this->contents;
-  }
-
-  /**
-   * appends $content to $this->contents
-   *
-   * @param mixed $content RenderableBlock or array of RenderablBlocks 
-   * @return void
-   **/
-  public function appendContent($content){
-    $content = ( is_array($content) ) ? $content : [$content];
-    foreach( $content as $idx => $item ){
-      array_push($this->contents, $item);
+    public function parse(\SimpleXMLElement $simpleXmlElement)
+    {
+        $this->setName($this->getAttribute($simpleXmlElement, 'name'));
+        parent::parse($simpleXmlElement);
+        // foreach children do the right thing
+        $this->setContents($this->parseContents($simpleXmlElement));
+        return $this;
     }
-    return $this;
-  }
 
-  /**
-   * prepends content to $this->contents
-   *
-   * @param mixed $content RenderableBlock or array of RenderablBlocks 
-   * @return void
-   **/
-  function prependContent($content){
-    $content = ( is_array($content) ) ? $content : [$content];
-    foreach( array_reverse($content) as $idx => $item ){
-      array_unshift($this->contents, $item);
+    public function parseContents(\SimpleXMLElement $simpleXmlElement)
+    {
+        $blockFactory = new BlockFactory();
+        $children = [];
+        foreach ($simpleXmlElement->children() as $child) {
+            if (in_array($child->getName(), $blockFactory->getWhitelist())) {
+                $childClass = $blockFactory->getBlockClass($child);
+                $childBlock = $childClass::createWithParameters($child, $this->getParameters());
+                if (!$childBlock->name) {
+                    $classParts = explode('\\', $childClass);
+                    $childBlock->name = uniqid(end($classParts).'-', false);
+                }
+                $children[$childBlock->name] = $childBlock;
+            }
+        }
+        return $children;
     }
-    return $this;
-  }
 
+    /**
+     * sets the name if any
+     *
+     * @return Sirs\Survey\Container $this
+     **/
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * gets name
+     *
+     * @return string
+     **/
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets the contents of the container
+     *
+     * @param array $contents - array of contents
+     * @return Sirs\Survey\Container $this
+     **/
+    public function setContents(array $contents)
+    {
+        $this->contents = $contents;
+        return $this;
+    }
+
+    /**
+     * Gets the contents of this container
+     *
+     * @return array - Array of contents
+     **/
+    public function getContents()
+    {
+        return $this->contents;
+    }
+
+    /**
+     * appends $content to $this->contents
+     *
+     * @param mixed $content RenderableBlock or array of RenderablBlocks
+     * @return void
+     **/
+    public function appendContent($content)
+    {
+        $content = (is_array($content)) ? $content : [$content];
+        foreach ($content as $idx => $item) {
+            array_push($this->contents, $item);
+        }
+        return $this;
+    }
+
+    /**
+     * prepends content to $this->contents
+     *
+     * @param mixed $content RenderableBlock or array of RenderablBlocks
+     * @return void
+     **/
+    public function prependContent($content)
+    {
+        $content = (is_array($content)) ? $content : [$content];
+        foreach (array_reverse($content) as $idx => $item) {
+            array_unshift($this->contents, $item);
+        }
+        return $this;
+    }
 }
