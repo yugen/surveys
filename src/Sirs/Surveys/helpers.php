b2a49901fa6a57920a864da9a88cd3df30690ea0
jward3 <jward3@email.unc.edu>
2018-01-18 10:19:36 -0500

    Fix helpers.php - getSurveyFormUrl

--- src/Sirs/Surveys/helpers.php
+++ src/Sirs/Surveys/helpers.php
@@ -1,23 +1,25 @@
 <?php
 
 if (! function_exists('class_response')) {
-	function class_response() {
-		return config('surveys.bindings.models.Response', Sirs\Surveys\Models\Response::class);
-	}
+    function class_response()
+    {
+        return config('surveys.bindings.models.Response', Sirs\Surveys\Models\Response::class);
+    }
 }
 
 if (! function_exists('class_survey')) {
-	function class_survey() {
-		return config('surveys.bindings.models.Survey', Sirs\Surveys\Models\Survey::class);
-	}
+    function class_survey()
+    {
+        return config('surveys.bindings.models.Survey', Sirs\Surveys\Models\Survey::class);
+    }
 }
 
 if (! function_exists('getSurveyFromUrl')) {
-	function getSurveyFormUrl($response, $pageName = null) {
-	    $url = '/'.strtolower(preg_replace('/\\\/', '-', get_class($response->respondent)));
-	    $url .= '/'.$response->respondent->id.'/survey/'.$response->survey->slug;
-	    $url .= ($pageName)? '?page'.$pageName : '';
-	    return $url;
-	}	
+    function getSurveyFormUrl($response, $pageName = null)
+    {
+        $url = '/'.strtolower(preg_replace('/\\\/', '-', get_class($response->respondent)));
+        $url .= '/'.$response->respondent->id.'/survey/'.$response->survey->slug;
+        $url .= ($pageName)? '?page='.$pageName : '';
+        return $url;
+    }
 }
-
