9b7971bdf26b8e8d0642213d44eb4f8f7f13ae73
jward3 <jward3@email.unc.edu>
2019-04-04 11:08:41 -0400

    Remove commented out code

--- src/Sirs/Surveys/Views/questions/text/default_text.blade.php
+++ src/Sirs/Surveys/Views/questions/text/default_text.blade.php
@@ -1,9 +1,3 @@
-{{-- @extends('surveys::questions.question')
-
-@section('answers')
-  @include('surveys::questions.input', ['question'=>$renderable, 'context'=>$context])
-@endsection --}}
-
 @component('surveys::questions.question', compact('renderable', 'context'))
   @slot('answers')
     @include('surveys::questions.input', ['question'=>$renderable, 'context'=>$context])
