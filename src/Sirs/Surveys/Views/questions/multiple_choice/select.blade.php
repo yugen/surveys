8aacddccefcc1f450a13f15fe11fc1eb94b0a1e5
jward3 <jward3@email.unc.edu>
2019-07-11 13:18:51 -0400

    Fix select.blade.php layout

--- src/Sirs/Surveys/Views/questions/multiple_choice/select.blade.php
+++ src/Sirs/Surveys/Views/questions/multiple_choice/select.blade.php
@@ -1,7 +1,7 @@
 @component('surveys::questions.question', compact('renderable', 'context'))
   @slot('answers')
     <div class="row">
-      <div class="question-answers col-sm-9">
+      <div class="question-answers">
         @include('surveys::questions.multiple_choice.select_input', ['question'=>$renderable, 'context'=>$context])
       </div>
     </div>
