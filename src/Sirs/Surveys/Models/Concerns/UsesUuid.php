8d379c922703a1cb781ed208d6b0d58ef97f4629
Brian Cass <brian.cass@unc.edu>
2020-02-26 22:31:26 -0500

    fixed unique check for uuid

--- src/Sirs/Surveys/Models/Concerns/UsesUuid.php
+++ src/Sirs/Surveys/Models/Concerns/UsesUuid.php
@@ -12,9 +12,16 @@ trait UsesUuid
             if (! $model->getKey()) {
                 // Get uuid and verify it's unique
                 $uuid = (string) Str::uuid();
-                /* while($model::where($model->getKeyName(), '=', $uuid)->exists()) {
-                    $uuid = (string) Str::uuid();
-                }*/
+
+                // Adding a unique check that is specific for the survey package
+                $responses = $model->survey->responses()->get('id');
+                foreach($responses as $response){
+                    if( $response->id == $uuid ){
+                        $uuid = (string) Str::uuid();
+                    }
+                }
+                // end unique check for survey package
+
                 $model->{$model->getKeyName()} = $uuid;
             }
         });
